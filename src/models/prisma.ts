9e414b6d73de652478001766ef267d0b8212ff04
Saurav Kumar <codewizard.saurav@gmail.com>
2025-05-22 12:45:51 +0530

    update prisma client path

--- src/models/prisma.ts
+++ src/models/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../prisma/generated/client';
 import logger from '../utils/logger';
 
 // Create a singleton instance of PrismaClient
